b3319f832f52def4719dfcfdd6b9e198dc1d8ac2
Sergey P <seregapanin23703@gmail.com>
2022-08-18 22:22:58 +0300

    Выполнил домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,33 @@
 public class Main {
     public static void main(String[] args) {
         generateRandomArray();
+        int[] arr = generateRandomArray();
 
 
-    }
-
-    //Первая задача
-    int[] arr = generateRandomArray();
-
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
+        //First exercise
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-            System.out.println(arr[i]);
-            //sum = sum+arr[i];    //1 й вариант
-        }
         for (int element : arr) {   // 2 й вариант
             sum += element;
         }
         System.out.println("Сумма трат за месяц составила " + sum);
         // Second exercise
         int maxSpendInTheMonth = -1;
+        int minSpendInTheDay = 1_000_000;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > maxSpendInTheMonth) {
                 maxSpendInTheMonth = arr[i];
 
             }
-        }
-        System.out.println("Максимальная сумма трат за день " + maxSpendInTheMonth);
-        /////
-        int minSpendInTheDay = 1_000_000;
-        for (int i = 0; i < arr.length; i++) {
             if (arr[i] < minSpendInTheDay) {
                 minSpendInTheDay = arr[i];
 
             }
+
         }
+        System.out.println("Максимальная сумма трат за день " + maxSpendInTheMonth);
         System.out.println("Минимальная сумма трат за день " + minSpendInTheDay);
+        /////
+
         //Third exercise
         double arrageSpend = sum * 1.0 / arr.length;
         System.out.println("«Средняя сумма трат за месяц составила" + arrageSpend + " рублей».");
@@ -46,10 +35,22 @@ public class Main {
 
         // four exercise
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length-1; 0 <=i ; i--) {
+        for (int i = reverseFullName.length-1; i >=0 ; i--) {
             System.out.print(reverseFullName[i]);
 
 
+        }
+
+    }
+
+    //Первая задача
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+            System.out.println(arr[i]);
         }
 
 
