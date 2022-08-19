356aec77bac03ae6e9814103372ac0c542e937bb
Sergey P <seregapanin23703@gmail.com>
2022-08-19 20:38:38 +0300

    Исправил расположение генерируещего метода

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-        generateRandomArray();
-        int[] arr = generateRandomArray();
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+            System.out.println(arr[i]);
+        }
+        return arr;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
 
         //First exercise
         int sum = 0;
@@ -34,26 +42,12 @@ public class Main {
 
 
         // four exercise
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length-1; i >=0 ; i--) {
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
 
-
         }
 
-    }
-
-    //Первая задача
-    public static int[] generateRandomArray() {
-        java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = random.nextInt(100_000) + 100_000;
-            System.out.println(arr[i]);
-        }
-
-
-        return arr;
     }
 
 
